2d75e2597da8499ac0277751dee3320f4b67bba6
José Adrián Garrido Puertas <jose.a.garrido.p@gmail.com>
2015-11-29 13:14:24 +0100

    Primero pasos para un script que controla la transformación del prota

---
+++ Game Jam 2015/Assets/Scripts/GhostChanges.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostChanges : MonoBehaviour {
+    private string state;
+    public Animator anima;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SetState(int newState) {
+        switch(newState)
+        {
+            case 0:
+                state = "Ghost";
+                break;
+            case 1:
+                state = "Sword";
+                break;
+            case 2:
+                state = "Shield";
+                break;
+            case 4:
+                state = "Goblin";
+                break;
+            default:
+                Debug.Log("NO PODEMOS CAMBIAR LA VARIABLE DE CONTROL DE CAMBIO");
+                break;
+        }
+    }
+
+    public void ChangeAnimation() {
+        switch (state)
+        {
+            case "Ghost":
+                //gameObject.GetComponent<Animator>().
+                break;
+            case "Sword":
+                
+                break;
+            case "Shield":
+                
+                break;
+            case "Goblin":
+                
+                break;
+            default:
+                Debug.Log("NO HEMOS REALIZADO LA ACCION DE CAMBIO");
+                break;
+        }
+
+    }
+
+}
