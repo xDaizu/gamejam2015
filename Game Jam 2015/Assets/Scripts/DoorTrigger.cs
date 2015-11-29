29008e04ffe5386808dec5977a4f8cff8e899d26
Daizu <danielparejom@gmail.com>
2015-11-29 04:19:58 +0100

    Funciona ya el puto collider del puto player; cojones ya.

--- Game Jam 2015/Assets/Scripts/DoorTrigger.cs
+++ Game Jam 2015/Assets/Scripts/DoorTrigger.cs
@@ -1,30 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DoorTrigger : MonoBehaviour {
+public class DoorTrigger : MonoBehaviour
+{
     public GameObject exitDoor;
 
+    private static bool justTP;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
-    void OnTriggerEnter2D(Collider2D other)
+    IEnumerator OnTriggerEnter2D(Collider2D other)
     {
+        //Debug.Log("Door OnTriggerEnter2D, name="+other.gameObject.name);
+
         if (other.gameObject.tag == "Player")
         {
-            // Movemos cámara a destino
+            Vector3 TargetPoint = exitDoor.transform.Find("SpawnPoint").gameObject.transform.position;
+            Debug.Log("Tag Player detectado, moviendo a x=" + TargetPoint.x + ", y= " + TargetPoint.y);
+            GameObject playerToMove = other.gameObject;
 
-            // Teletransportamos muñeco
-            other.gameObject.SetActive(false);
-            other.gameObject.transform.position = exitDoor.transform.position;
-            other.gameObject.SetActive(true);
+            playerToMove.SetActive(false);
+            playerToMove.transform.position = TargetPoint;
+            yield return new WaitForSeconds(0.01f);
+            playerToMove.SetActive(true);
+
+        }
+
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        //Debug.Log("Door OnTriggerLeave");
+        if (other.gameObject.tag == "Player")
+        {
+
         }
     }
+
 }
