d2970bdd90ce988c5fa7d6beec301f8091755290
José Adrián Garrido Puertas <jose.a.garrido.p@gmail.com>
2015-11-29 02:44:43 +0100

    El cono de visión reconoce pero no gira

--- Game Jam 2015/Assets/Scripts/ColliderTestScript.cs
+++ Game Jam 2015/Assets/Scripts/ColliderTestScript.cs
@@ -5,34 +5,46 @@ using System.Collections;
 
 public class ColliderTestScript : MonoBehaviour
 {
-
+    public float rotationSpeed = 1f;
     public bool scriptActivado;
     public string scriptTag;
     public Enemy enemyScript;
+
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            float x, y, z;
+            GameObject player = other.gameObject;
+            x = player.transform.position.x + transform.position.x;
+            y = player.transform.position.y + transform.position.y;
+            //z = player.transform.position.z + transform.position.z;
+            Vector3 coordinates = new Vector3(x,y,0f);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                                Quaternion.LookRotation(coordinates),
+                                rotationSpeed * Time.deltaTime);
+        }
+    }
+
     public void OnTriggerEnter2D(Collider2D other)
     {
-        if (scriptActivado)
-            Debug.Log("Trigger enter de " + scriptTag);
-
-        //enemyObject = transform.parent.gameObject;
-
         if (other.tag == "Player")
         {
             GameObject player = other.gameObject;
             enemyScript.playerSighted(player);
         }
+ 
     }
-    public void OnTriggerLeave2D(Collider2D other)
+    public void OnTriggerExit2D(Collider2D other)
     {
-        if (scriptActivado)
-            Debug.Log("Trigger enter de " + scriptTag);
-
-        //enemyObject = transform.parent.gameObject;
-
         if (other.tag == "Player")
         {
             GameObject player = other.gameObject;
             enemyScript.playerLost(player);
         }
     }
+
+    public void GetSpeedRotation(float speed) { this.rotationSpeed = speed; }
+    public float SetSpeedRotation() { return rotationSpeed; }
 }
--- Game Jam 2015/Assets/Scripts/Enemy.cs
+++ Game Jam 2015/Assets/Scripts/Enemy.cs
@@ -2,13 +2,15 @@
 using System.Collections;
 
 public class Enemy : MonoBehaviour {
-    public float speed = 5f;
+    public float speed = 1f;
     public GameObject objeto;
     public float distance, radioAtaque;
     public Animator anima;
-	
+    private bool aggresiveMode = false;
+    private GameObject target = null;
+
     // Use this for initialization
-	void Start () {
+    void Start () {
         anima = gameObject.GetComponent<Animator>();
         distance = Vector3.Distance(gameObject.transform.position, objeto.transform.position);
     }
@@ -16,7 +18,24 @@ public class Enemy : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         distance = Vector3.Distance(gameObject.transform.position, objeto.transform.position);
+        if (aggresiveMode) {
+            this.aggresiveAction();
+        }
        
+    }
+
+    void aggresiveAction() {
+        if (distance > radioAtaque)
+        {
+            anima.SetFloat("Velocidad", speed);
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, target.transform.position, speed * Time.deltaTime);
+            //gameObject.transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        if (distance <= radioAtaque)
+        {
+            anima.SetBool("Ataca", true);
+        }
+
     }
 
     public void SetSpeed(float sp) { this.speed=sp; }
@@ -29,17 +48,16 @@ public class Enemy : MonoBehaviour {
     public GameObject GetObjetivo() { return this.objeto; }
 
     public void playerSighted(GameObject player) {
-        if (distance > radioAtaque)
-        {
-            anima.SetFloat("Velocidad", speed);
-            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, objeto.transform.position, speed * Time.deltaTime);
-        }
-        if (distance <= radioAtaque)
-        {
-            anima.SetBool("Ataca", true);
-        }
+        this.aggresiveMode = true;
+        this.target = player;
+
+      
     }
-    public void playerLost(GameObject player) { }
+    public void playerLost(GameObject player)
+    {
+        this.aggresiveMode = false;
+        this.target = null;
+    }
 
     public void OnTriggerEnter2D(Collider2D colision)
     {
